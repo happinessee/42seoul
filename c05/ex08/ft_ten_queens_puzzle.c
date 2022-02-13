816181e06a871120d42a20380fa0dca3507f8741
happinessee <gygur1206@gmail.com>
2022-02-13 21:58:57 +0900

    c5 ex08ing...

--- c05/ex08/ft_ten_queens_puzzle.c
+++ c05/ex08/ft_ten_queens_puzzle.c
@@ -6,11 +6,38 @@
 /*   By: hyojeong <hyojeong@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 01:13:42 by hyojeong          #+#    #+#             */
-/*   Updated: 2022/02/12 01:14:51 by hyojeong         ###   ########.fr       */
+/*   Updated: 2022/02/13 21:58:44 by hyojeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	judge(int x, char *board)
+{
+	int		i;
+	int		tmp;
+
+	i = 0;
+	while (i < x)
+	{
+		tmp = board[x] - board[i];
+		if (tmp < 0)
+			tmp *= -1;
+		if (board[x] == board[i] || x - i == tmp)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	queen(int x, int *cnt)
+{
+	if (x == 10)
+	{
+
+
 int	ft_ten_queens_puzzle(void)
 {
+	int		cnt;
+
+	cnt = 0;
 	return(0);
 }
