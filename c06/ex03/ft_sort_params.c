6dd139763443f89dbbea0ed0c247b661b19580bd
Hyohyeok Jeong <hyojeong@c8r7s3.42seoul.kr>
2022-02-14 17:47:45 +0900

    c06 complete...

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: hyojeong <hyojeong@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 15:06:11 by hyojeong          #+#    #+#             */
-/*   Updated: 2022/02/14 15:35:00 by hyojeong         ###   ########.fr       */
+/*   Updated: 2022/02/14 17:45:37 by hyojeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,33 +22,60 @@ void	ft_print(char *str)
 	write(1, "\n", 1);
 }
 
-void	ft_swap(char *str1, char *str2)
+void	ft_swap(char **str1, char **str2)
 {
 	char	*tmp;
 
-	tmp = str1;
-	str1 = str2;
-	str2 = tmp;
+	tmp = *str1;
+	*str1 = *str2;
+	*str2 = tmp;
 }
 
-void	ft_sort_check(char *str1, char *str2)
+int	ft_strcmp(char *s1, char *s2)
 {
 	int		cnt;
 
 	cnt = 0;
-	while (str1[cnt] && str2[cnt])
+	while (s1[cnt] && s2[cnt])
 	{
-		if (str1[cnt] > str2[cnt])
-			return (1);
-		else if (str1[cnt] == str2[cnt])
-			cnt ++;
-		else
-			return (0);
+		if (s1[cnt] != s2[cnt])
+		{
+			if (s1[cnt] > s2[cnt])
+				return (1);
+			else
+				return (0);
+		}
+		cnt++;
 	}
+	if (s1[cnt] > s2[cnt])
+		return (1);
+	else
+		return (0);
 }
 
 int	main(int argc, char **argv)
 {
+	int		cnt;
+	int		cnt2;
+	int		index;
+
+	index = 1;
+	cnt = 1;
+	cnt2 = 1;
 	if (argc > 1)
 	{
-
+		while (cnt < argc)
+		{
+			while (cnt2 < argc - 1)
+			{
+				if (ft_strcmp(argv[cnt2], argv[cnt2 + 1]))
+					ft_swap(&argv[cnt2], &argv[cnt2 + 1]);
+				cnt2 ++;
+			}
+			cnt2 = 0;
+			cnt ++;
+		}
+		while (index < argc)
+			ft_print(argv[index++]);
+	}
+}
